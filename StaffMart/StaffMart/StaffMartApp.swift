aeebf4c2e95222f8f66c3c9fbf4ec0da1251bb59
aaa <aaa@aaa-no-MacBook-Air.local>
2025-03-13 23:11:48 +0900

    initial commit

---
+++ StaffMart/StaffMart/StaffMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaffMartApp.swift
+//  StaffMart
+//
+//  Created by aaa on 2025/03/13.
+//
+
+import SwiftUI
+
+@main
+struct StaffMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
